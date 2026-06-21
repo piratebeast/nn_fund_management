bc1c09686529e474f6ab5e63b5d9dad7bfc21ae4
Mosharrof Hossian Shishir <012shishir@gmail.com>
2026-06-21 17:49:07 +0600

    initial commit

---
+++ __manifest__.py
@@ -0,0 +1,10 @@
+{
+    'name': 'NN Fund Management',
+    'version': '1.0',
+    'category': 'Accounting',
+    'summary': 'Manage funds professionally.',
+    'depends': ['base'],
+    'data': [],
+    'installable': True,
+    'application': True,
+}
